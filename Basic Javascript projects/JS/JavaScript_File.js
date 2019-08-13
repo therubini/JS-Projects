35507ddff8bb059e6b99fe83056d9a4dd1c107f8
Jonathan Rubini <53010849+therubini@users.noreply.github.com>
2019-08-13 11:43:16 -0400

    Pushed JS #58

---
+++ Basic Javascript projects/JS/JavaScript_File.js
@@ -0,0 +1,14 @@
+//document.write("Hello World!"); writes in body
+//alrt("Hello World!"); pop up
+var a="This is a string.", b="This is a string with quotation\'s in it, \"Hello World!\""+" I\'m concatenating"+" these elements."+" ";
+var b=b.fontcolor("green");
+var c="This is concatenating with color!"+" "+"This is 3+3 added"+ " ";
+var c=c.fontcolor("red");
+window.alert(a);
+document.write(b,c);
+document.write(3+3); //---this will perform the function of 3+3. (notice no quotations)
+function my_first_function() {
+    var string="This is text!";
+    document.write(string);
+}
+
---
+++ Basic Javascript projects/JS/celToFer.js
@@ -0,0 +1,17 @@
+function convertToCelsius(tempInF) {
+    tempInC=(tempInF - 32) * (5/9);
+    return tempInC
+}
+
+console.log( convertToCelsius(200) );
+//enter values in console.log
+console.log(56+35);
+
+//add w/function
+function add(num1,num2) {
+    return num1 + num2;
+}
+console.log(add(56,35));
+
+//or sum=add(56+35);/function add()
+//console.log(sum);
